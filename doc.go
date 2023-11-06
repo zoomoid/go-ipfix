e0702c73825d55d785c049000b6b807066b153f0
Alexander Bartolomey <occloxium@gmail.com>
2023-11-06 17:48:01 +0100

    docs: more background documentation for IPFIX
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- doc.go
+++ doc.go
@@ -16,7 +16,12 @@ limitations under the License.
 
 /*
 Package for working with IPFIX messages. Supports decoding and encoding from and to IPFIX according
-to RFC 7011. Additionally, also supports most other major IPFIX RFCs, namely
+to RFC 7011.
+
+# Overview
+
+IPFIX message format is defined in RFC 7011. go-ipfix implements decoding and encoding of messages adhering
+to that RFC. Additionally, most other major IPFIX RFCs are also supported feature-wise, namely
 
 - RFC 5103: Bidirectional Flow Export Using IP Flow Information Export (IPFIX)
 
@@ -45,5 +50,31 @@ using Go's rich net.PacketConn tooling, IPFIX's prescription of how collection v
 works is a bit more involved and we decided to include our contribution of such a TCP listener in
 this package. The below example shows how to use it, and in particular shows how the TCP and UDP listener API
 is identical.
+
+# Data Structures
+
+IPFIX messages are nested to contain as much possible data in a single message as possible.
+On a top-level, an IPFIX message contains 3 kinds of typed sets of records, namely template sets, data sets, and
+options template sets. These sets have a header-like data structure at the start denoting the "ID" of the set.
+In the case of template records, the ID is expected to be 2, for options template sets it is 3, and data sets are freely
+taken from 255 up to 65535. The IDs of data sets are associated with IDs defined in previously sent template records.
+
+Each set contains one or more records of the same type, i.e., template records, data records, options template records.
+
+Each record contains one or more fields, whose semantics are defined either in the standard registry 0 of IANA-IPFIX, or
+vendor-specific/proprietary are Information Elements.
+
+Values of such fields are typed according to RFC 7011 or RFC 6313 (which defines higher-level data types). In go-ipfix,
+all those data types implement the DataType interface. For more details on the data types consult the corresponding RFCs.
+
+Notably, RFC 6313 defines mechanisms for (recursively) nesting records in data records. The SubTemplateList and SubTemplateMultiList
+data types allow for nesting data records created from *possibly different* templates than the containing data set, thus creating
+a tree-like structure, where records can be nested up to a certain extent (not exceeding the maximum message size of 2^16-1 bytes minus a couple of bytes for headers).
+
+The coupling of templates and records (read: data semantics are detached from the actual data) requires a stateful management of
+the templates such that incoming data records can be decoded if and only if the corresponding template was received before. RFC 7011
+prescribes that decoders (collectors) may store records not able to decode due to absense of the template up to a certain point if e.g.
+asymmetric paths create such a race condition. go-ipfix *does not* implement such a system, but the error returned from the Decoder if
+a template is not known can be used to queue up such messages and work off that queue at any later point.
 */
 package ipfix
